4e27ae396230145e2b4fffde9b4633db31284f90
arhodia <arhodia30@gmail.com>
2022-06-01 15:16:42 +0300

    change the name of the file

--- App.java
+++ App.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
-public class main {
+public class App {
     public static void main(String[] args) throws Exception {
+
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter Biographic: ");
