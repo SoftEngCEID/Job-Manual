cb06280e2185c2684c14637b3ec1fcb9bc7969e5
arhodia <arhodia30@gmail.com>
2022-06-01 20:20:08 +0300

    updates on class company description

--- Company_description.java
+++ Company_description.java
@@ -1,9 +1,17 @@
 public class Company_description {
-    String description;
+    String description ;
 
     public Company_description(String description) {
         this.description = description;
 
+    }
+    
+
+    public void setdescription(String descr) {
+        this.description = descr;
+    }
+    public String getdescription() {
+        return description;
     }
 
 }
