3593630c5ae83a1365897469835b96abbd145474
arhodia <arhodia30@gmail.com>
2022-06-05 21:46:06 +0300

    changes

--- App.java
+++ App.java
@@ -14,7 +14,7 @@ public class App {
         String userprfl2 = user2.getprofile_category();
         String userprfl3 = user3.getprofile_category();
 
-        switch (userprfl3) {
+        switch (userprfl2) {
 
             case "candidate":
                 Scanner myObj = new Scanner(System.in); //
@@ -29,11 +29,52 @@ public class App {
                 // System.out.println(bio.getBio());
                 // System.out.println(bio.getis_public());
 
-                boolean tr = bio.getis_public();
-                if (tr) {
+                boolean check = bio.getis_public();
+                if (check) {
                     System.out.println("Your bio is public\n");
                 } else {
                     System.out.println("Your bio is private");
+                }
+                Scanner myedit = new Scanner(System.in);
+                System.out.println("Do yo want to edit your account...press true");
+                boolean edit = myedit.nextBoolean();
+                if (edit) {
+
+                    Scanner myname = new Scanner(System.in);
+                    System.out.println("Init/change your name");
+                    String initname = myname.nextLine();
+
+                    Scanner mylastname = new Scanner(System.in);
+                    System.out.println("Init/change your last name");
+                    String initlastname = mylastname.nextLine();
+
+                    Scanner myadress = new Scanner(System.in);
+                    System.out.println("Init/change your address");
+                    String initmyadress = myadress.nextLine();
+
+                    Scanner mybirthdate = new Scanner(System.in);
+                    System.out.println("Init/change your birth_date");
+                    String initmybirthdate = mybirthdate.nextLine();
+
+                    Scanner mynationality = new Scanner(System.in);
+                    System.out.println("Init/change your nationality");
+                    String initmynationality = mynationality.nextLine();
+
+                    Scanner myphonenumber = new Scanner(System.in);
+                    System.out.println("Init/change your phone_number");
+                    int initmyphonenumber = myphonenumber.nextInt();
+
+                    Candid_User candidUser = new Candid_User(initname, initlastname, initmyadress, initmybirthdate,
+                            initmynationality, initmyphonenumber, newUsername, ,, );
+                    candidUser.Insert_name(initname);
+                    candidUser.Insert_lastname(initlastname);
+                    candidUser.Insert_adress(initmyadress);
+                    candidUser.Insert_newBirth(initmybirthdate);
+                    candidUser.Insert_newnationality(initmynationality);
+                    candidUser.Insert_newphone_number(initmyphonenumber);
+
+                } else {
+                    System.out.println("..................");
                 }
 
             case "employer":
@@ -50,3 +91,29 @@ public class App {
     }
 
 }
+
+/*
+ * Scanner myuser = new Scanner(System.in);
+ * 
+ * System.out.println("choose ");
+ * int choice = myuser.nextInt();
+ * 
+ * 
+ * switch(choice) {
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * }
+ */
--- User.java
+++ User.java
@@ -29,61 +29,93 @@ public class User {
         } else if (this.profile_category == "employer") {
 
         } else {
-            System.out.println("There ara only two categories.Please select ");
+            System.out.println("There are only two categories.Please select ");
         }
 
     }
 
     public String getprofile_category() {
         return profile_category;
+
     }
 
 }
 
 class Candid_User extends User {
-    public Candid_User(String username, String password, String email, String profile_category) {
+
+    private String name, last_name, address, birth_date, nationality;
+    private int phone_number;
+
+    public Candid_User(String name, String last_name, String address, String birth_date, String nationality,
+            int phone_number, String username, String password, String email, String profile_category) {
         super(username, password, email, profile_category);
+
     }
 
-    private String name, last_name, address, birth_date, nationality, phone_number;
-
-    public void Insert_data(String newName, String newLast, String newAdd, String newBirth, String newNation,
-            String newPhone) {
+    public void Insert_name(String newName) {
         this.name = newName;
+    }
+
+    public void Insert_lastname(String newLast) {
+
         this.last_name = newLast;
+
+    }
+
+    public void Insert_adress(String newAdd) {
         this.address = newAdd;
+    }
+
+    public void Insert_newBirth(String newBirth) {
         this.birth_date = newBirth;
-        this.nationality = newNation;
-        this.phone_number = newPhone;
+    }
+
+    public void Insert_newnationality(String newnationality) {
+        this.nationality = newnationality;
+    }
+
+    public void Insert_newphone_number(int newphone_number) {
+        this.phone_number = newphone_number;
     }
 
     public String Get_Data() {
         String s = "Name: " + name + "\nLast name: " + last_name + "\nAddress: " + address + "\nBirthday: " + birth_date
-                + "\nNationality: " + nationality + "\nPhone number: " + phone_number;
+                + "\nNationality: " + nationality;
+
         return s;
     }
 
-    public void Insert_Bio() {
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter Bio: ");
+    public int Get_phone() {
 
-        String bio1 = myObj.nextLine();
+        System.out.println("Phone :");
+        return phone_number;
 
-        System.out.println("Do you want it to be public?\nYES: Press 1\tNO: Press 2");
-        int choice = myObj.nextInt();
-        boolean istrue;
-        switch (choice) {
-            case 1:
-                istrue = true;
-            case 2:
-                istrue = false;
-            default:
-                istrue = false;
-        }
-
-        // Biography bio = new Biography(bio1, is
-        // System.out.println(bio.getBio());
-        // System.out.println(bio.getis_Public());
     }
+
+    /*
+     * public void Insert_Bio() {
+     * Scanner myObj = new Scanner(System.in);
+     * System.out.println("Enter Bio: ");
+     * 
+     * String bio1 = myObj.nextLine();
+     * 
+     * System.out.println("Do you want it to be public?\nYES: Press 1\tNO: Press 2"
+     * );
+     * int choice = myObj.nextInt();
+     * boolean istrue;
+     * switch (choice) {
+     * case 1:
+     * istrue = true;
+     * case 2:
+     * istrue = false;
+     * default:
+     * istrue = false;
+     * }
+     * 
+     * // Biography bio = new Biography(bio1, is
+     * // System.out.println(bio.getBio());
+     * // System.out.println(bio.getis_Public());
+     * }
+     */
 
 }
