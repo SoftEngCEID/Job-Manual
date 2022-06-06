206b74feb0a1feac37d6f8125f00ba8c9275da07
arhodia <arhodia30@gmail.com>
2022-06-07 00:10:39 +0300

    publish job

--- ucmain.java
+++ ucmain.java
@@ -14,13 +14,31 @@ public class ucmain {
                 Scanner myoption = new Scanner(System.in);
                 System.out.println(
                         "Choose 1 if you want to publish job \n 2 if you want to edit \n 3 if you want to delete a job ");
-                int optnumber = myObj.nextInt();
+                int optnumber = myoption.nextInt();
                 switch (optnumber) {
                     case 1:
+                        ArrayList<String> jobs = new ArrayList<String>();
+                        boolean checklist = jobs.isEmpty();
+                        if (checklist) {
+                            Scanner insertjobtitle = new Scanner(System.in);
+                            System.out.println(
+                                    "Enter job title");
+                            String myjobtitle = insertjobtitle.nextLine();
+                            Scanner insertjobdescr = new Scanner(System.in);
+                            System.out.println(
+                                    "Enter job description");
+                            String myjobdescr = insertjobdescr.nextLine();
+                            jobs job1 = new jobs(myjobtitle, myjobdescr);
+                            job1.setDescription(myjobdescr);
+                            job1.setJob_title(myjobtitle);
+                            jobs.add(myjobdescr);
+                            System.out.println(jobs);
+                        } else {
 
+                        }
                         break;
                     case 2:
-
+                        jobs job1 = new jobs("Software Engineer Position", "HI, we looking for a java developer ");
                         break;
                     case 3:
 
