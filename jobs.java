a62096f746758e350f91f90e92953eda453747c8
arhodia <arhodia30@gmail.com>
2022-06-06 19:15:21 +0300

    create class for jobs description

---
+++ jobs.java
@@ -0,0 +1,27 @@
+public class jobs {
+    private String Job_title;
+    private String Description;
+
+    public jobs(String Job_title, String Description) {
+        this.Job_title = Job_title;
+        this.Description = Description;
+    }
+
+    public void setJob_title(String Bio) {
+        this.Job_title = Job_title;
+
+    }
+
+    public String getJob_title() {
+        return Job_title;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public Boolean getDescription() {
+        return Description;
+    }
+
+}
--- ucmain.java
+++ ucmain.java
@@ -1,9 +1,37 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class ucmain {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Type 1 if you want to edit <My Jobs> or 2 if you want to exit ");
+        int number = myObj.nextInt();
+
+        switch (number) {
+            case 1:
+                Scanner myoption = new Scanner(System.in);
+                System.out.println(
+                        "Choose 1 if you want to publish job \n 2 if you want to edit \n 3 if you want to delete a job ");
+                int optnumber = myObj.nextInt();
+                switch (optnumber) {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                }
+
+                break;
+            case 2:
+                break;
+
+        }
 
     }
 }
