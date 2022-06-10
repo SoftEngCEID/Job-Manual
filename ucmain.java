2819c17e8a672ab1cadd8f928809e93c7e903ed3
arhodia <arhodia30@gmail.com>
2022-06-10 19:57:15 +0300

    in process alternative choide 1

--- ucmain.java
+++ ucmain.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class ucmain {
-    public static void main(String[] args) {
+    public static <plan> void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Type 1 if you want to edit <My Jobs> or 2 if you want to exit ");
         int number = myObj.nextInt();
@@ -13,11 +13,12 @@ public class ucmain {
             case 1:
                 Scanner myoption = new Scanner(System.in);
                 System.out.println(
-                        "Choose 1 if you want to publish job \n 2 if you want to edit \n 3 if you want to delete a job ");
+                        "Choose:\n 1 If you want to publish job \n 2 If you want to edit \n 3 If you want to delete a job");
                 int optnumber = myoption.nextInt();
                 switch (optnumber) {
                     case 1:
-                        ArrayList<String> jobs = new ArrayList<String>();
+
+                        HashMap<String, String> jobs = new HashMap<String, String>();
                         boolean checklist = jobs.isEmpty();
                         if (checklist) {
                             Scanner insertjobtitle = new Scanner(System.in);
@@ -31,9 +32,20 @@ public class ucmain {
                             jobs job1 = new jobs(myjobtitle, myjobdescr);
                             job1.setDescription(myjobdescr);
                             job1.setJob_title(myjobtitle);
-                            jobs.add(myjobdescr);
+                            jobs.put("myjobtitle", "myjobdescr");
                             System.out.println(jobs);
                         } else {
+                            user_plan usrpl = new user_plan("Standard");
+                            String userplan = usrpl.getplan();
+                            switch (userplan) {
+                                case "Standard":
+                                    System.out.println(
+                                            "Your plan is standard you can insert another job.Please update your plan");
+                                    break;
+                                case "Premium":
+                                    break;
+
+                            }
 
                         }
                         break;
---
+++ user_plan.java
@@ -0,0 +1,18 @@
+public class user_plan {
+    private String plan;
+
+    public user_plan(String plan) {
+        this.plan = plan;
+
+    }
+
+    public void setplan(String plan) {
+        this.plan = plan;
+
+    }
+
+    public String getplan() {
+        return plan;
+    }
+
+}
