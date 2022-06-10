fd32f38d762a9dbbfeabf15b55fce04095654f9d
arhodia <arhodia30@gmail.com>
2022-06-10 19:57:54 +0300

    change the variable of method getdescription

--- jobs.java
+++ jobs.java
@@ -20,7 +20,7 @@ public class jobs {
         this.Description = Description;
     }
 
-    public Boolean getDescription() {
+    public String getDescription() {
         return Description;
     }
 
